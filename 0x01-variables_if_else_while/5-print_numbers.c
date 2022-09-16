23d689e2c4d77d7ad3903610fbd6e27d1d458a4f
LeloM101 <lelomalinga101@gmail.com>
2022-09-16 14:33:04 -0700

    print single digits for base 10s

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,20 @@
+#include <time.h>
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+* main - entry point
+*
+* Return: Always 0 (success)
+*/
+
+int main(void)
+{
+int i
+
+for (i = 0; i < 10; i++)
+printf("%d", i);
+print ("\n");
+
+return (0);
+}

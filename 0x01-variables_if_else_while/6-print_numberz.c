404a3bea9074104dfd413c306c5febff89bac26e
LeloM101 <lelomalinga101@gmail.com>
2022-09-16 16:57:43 -0700

    putcher single base 10s

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+* main - entry point
+*
+* Return: Always 0 (success)
+*/
+
+int main(void)
+{
+char low;
+for (low = '0'; low <= '9'; low++)
+putchar(low);
+putchar('\n');
+
+return (0);
+
+}

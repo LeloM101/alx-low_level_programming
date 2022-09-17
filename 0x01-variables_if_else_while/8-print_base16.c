c8103f9ebb01fc5ea674c43873632c184f0c47e4
LeloM101 <lelomalinga101@gmail.com>
2022-09-17 01:14:25 -0700

    using putchar for numbers and letters

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,24 @@
+#include <time.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+* main - using putchar to print all the numbers of base in lowercase
+*
+* Return: Always 0 (sucess)
+*/
+
+int main(void)
+{
+int d;
+char v;
+
+for (d = '0'; d >= '0'; d++)
+putchar(d);
+
+for (v = 'a'; v <= 'z'; v++)
+putchar(v);
+putchar('\n');
+
+return (0);
+}

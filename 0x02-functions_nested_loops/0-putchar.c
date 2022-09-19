34e7bf8f0f972669562d608e62dfebf344bfec46
LeloM101 <lelomalinga101@gmail.com>
2022-09-19 11:43:31 -0700

    my readme file

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,6 +1,4 @@
-#include <stdio.h>
-#include <time.h>
-#include <main.h>
+#include "main.h"
 
 /**
 * main - printing word with putchar
@@ -10,7 +8,15 @@
 
 int main(void)
 {
-putchar("_putchar")
+_putchar("_");
+_putchar("p");
+_putchar("u");
+_putchar("t");
+_putchar("c");
+_putchar("h");
+_putchar("a");
+_putchar("r");
+_putchar("\n");
 
-return (0)
+return (0);
 }

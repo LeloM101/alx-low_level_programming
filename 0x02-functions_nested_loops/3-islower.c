22e60d64cb5debde55572080f6d661bcb2ed33d8
LeloM101 <lelomalinga101@gmail.com>
2022-09-21 15:10:42 -0700

    fixed

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -3,6 +3,7 @@
 * _islower - check description
 * Description: check for lowercase characters
 * @c: a character argument
+* Return: returns 1 and o depending on condition
 */
 int _islower(int c)
 {

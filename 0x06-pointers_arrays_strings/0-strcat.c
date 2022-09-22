c5f7ba3b752ad14f4a074cee4ed11c615c7fa0bc
LeloM101 <lelomalinga101@gmail.com>
2022-09-22 02:06:41 -0700

    correction

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -6,13 +6,14 @@
 * @src: an input string
 * Return: A pointer to the resulting string
 */
-{
+
 char *_strcat(char *dest, char *src)
-char *temp = dest;
-while (*dest)
-dest++;
-while (*src)
-*dest++ = *src++;
-*dest = '\0';
+{
+int index = 0, dest_len = 0;
+
+while (dest{index++})
+dest_len++;
+for (index = 0; src[index]; index++)
+dest[dest_len++] = src[index];
 return (temp);
 }

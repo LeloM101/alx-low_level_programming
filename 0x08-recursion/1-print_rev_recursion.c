b16348a9fa54789578ea145dcbe2c071883173de
LeloM101 <lelomalinga101@gmail.com>
2022-09-29 02:15:03 -0700

    fixed

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -1,6 +1,7 @@
 #include "main.h"
 /**
-* _print_rev_recursio - prints string in reverse
+* _print_rev_recursion - prints string in reverse
+* description: prints string in reverse
 * @s: string to be printed
 * Return: void
 */

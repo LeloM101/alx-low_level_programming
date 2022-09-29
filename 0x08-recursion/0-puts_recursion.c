d96463da953ff06afcaca7a75e0e22152cd9d4a4
LeloM101 <lelomalinga101@gmail.com>
2022-09-28 18:39:54 -0700

    hot fix

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -6,13 +6,15 @@
 * Return: Always 0 (Success)
 */
 
-void _puts_recursion(char *s)
+void _puts_recursion(char *s);
 {
-if (*s)
+if (*s == '\0')
 {
-_putchar(*s);
-_puts_recursion(s+1);
+_putchar('\n');
 }
 else
-_putchar('\n')
+{
+_putchar(s[0]);
+_puts_recursion(s + 1);
+}
 }

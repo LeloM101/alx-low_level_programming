4f576e69688b2fa9795fba29a0c897e2c1bb710d
LeloM101 <lelomalinga101@gmail.com>
2022-10-25 11:46:57 -0700

    task 5

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+* more_numbers - prints 0 to 14 ten times
+* Return: 0
+*/
+void more_numbers(void)
+{
+int a, i;
+for (i = 0; i <= 9; i++)
+{
+for (a = 0; a <= 14; a++)
+{
+if (a > 9)
+_putchar((a / 10) + '0');
+_putchar(a % 10 + '0');
+}
+_putchar('\n');
+}
+}

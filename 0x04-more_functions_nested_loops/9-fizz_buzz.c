ef306240a02bd54a2f60c16aa26b47a2ad7caac4
LeloM101 <lelomalinga101@gmail.com>
2022-10-26 05:07:48 -0700

    hit fix

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -11,11 +11,11 @@ int i = 1;
 for (i = 1; i < 100; i++)
 {
 if (i % 3 == 0 && i % 5 != 0)
-printf("Fizz ");
+printf("Fizz");
 if (i % 5 == 0 && i % 3 != 0)
-printf("Buzz ");
+printf("Buzz");
 if (i % 3 == 0 && i % 5 == 0)
-printf("FizzBuzz ");
+printf("FizzBuzz");
 else if (i % 3 != 0 && i % 5 != 0)
 printf("%d", i);
 printf(" ");

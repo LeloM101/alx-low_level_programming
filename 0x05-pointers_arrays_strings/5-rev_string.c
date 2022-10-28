5e03904cb26d282453e6ad06a1dddb565b597c9f
LeloM101 <lelomalinga101@gmail.com>
2022-10-28 13:24:01 -0700

    task 5

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+* rev_string - a function that reverse a string
+* followed by a new line.
+* @s: an input string
+* Return: Nothing
+*/
+void rev_string(char *s)
+{
+int len = 0;
+while (s[len] != '\0')
+len++;
+for (len = len - 1; len >= 0; len--)
+_putchar(s[len]);
+}

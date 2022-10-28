93e1d26538a5534caef8db8e5b1f95e100d3c519
LeloM101 <lelomalinga101@gmail.com>
2022-10-28 13:32:52 -0700

    fixed

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -7,9 +7,14 @@
 */
 void rev_string(char *s)
 {
-int len = 0;
+int len = 0, i = 0;
+char aux;
 while (s[len] != '\0')
 len++;
-for (len = len - 1; len >= 0; len--)
-_putchar(s[len]);
+while (i < len--)
+{
+aux = s[i];
+s[i++] = s[len];
+s[len] = aux;
+}
 }

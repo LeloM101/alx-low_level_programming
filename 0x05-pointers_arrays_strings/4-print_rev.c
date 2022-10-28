c76bbdd17bb1616977fcac68f9f12f30c1c53c33
LeloM101 <lelomalinga101@gmail.com>
2022-10-28 13:09:06 -0700

    hot fix

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -11,7 +11,7 @@ void rev_string(char *s)
 int len = 0;
 while (s[len] != '\0')
 len++;
-while (len)
-_putchar(s[--len]);
+for (len = len - 1; len >= 0; len--)
+_putchar(s[len]);
 _putchar('\n');
 }

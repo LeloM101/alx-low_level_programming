b78d883a9ce182e90fcd9d0d2d446d90040c20ca
LeloM101 <lelomalinga101@gmail.com>
2022-11-08 21:50:35 -0800

    hot fix

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -8,10 +8,13 @@
 */
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
-unsigned int i;
-unsigned char *d = dest;
-const unsigned char *s = src;
-for (i = 0; i < n; i++)
-d[i] = s[i];
+char *d = dest;
+while (n--)
+{
+*dest = *src;
+src++;
+dest++;
+}
 return (dest);
 }
+
